9aa0ff4748bb9a8ba51d1da9eacb8facadf04a52
akweaver32 <61985701+akweaver32@users.noreply.github.com>
2021-06-03 11:57:49 -0700

    counties

--- cocci/cocci_Test.R
+++ cocci/cocci_Test.R
@@ -1,5 +1,21 @@
 #install packages
 install.packages("sf")
 
-cocci <- c(3,3,3,3,3)
 #library
+library("sf") 
+library("ggplot2")
+
+#read in CA shapefile
+CA_aoi <- st_read("ca-state-boundary/CA_State_TIGER2016.shp")
+CA_counties_aoi <- st_read("CA_Counties/CA_Counties_TIGER2016.shp")
+
+#plot CA
+ggplot() +
+  geom_sf(data = CA_aoi, size = 3, color = "black", fill = "cyan1") +
+  coord_sf()
+
+#plot CA counties
+ggplot() +
+  geom_sf(data = CA_counties_aoi, size = 3, color = "black", fill = "cyan1") +
+  coord_sf()
+
