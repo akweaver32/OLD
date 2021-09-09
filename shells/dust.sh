dbecd1f6d278d94dd3a7cd66235ff5f72d2a8be5
akweaver32 <61985701+akweaver32@users.noreply.github.com>
2021-09-09 15:55:26 -0700

    shells for computing

---
+++ shells/dust.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=drop_cases
+#
+# Account:
+#SBATCH --account=fc_envids
+#
+# Partition:
+#SBATCH --partition=savio2_htc
+#
+# Request one node:
+#SBATCH --nodes=1
+#
+# Specify task:
+#SBATCH --ntasks-per-node=1
+#
+# Number of processors for threading:
+#SBATCH --cpus-per-task=1
+#
+# Wall clock limit:
+#SBATCH --time=12:00:00
+#
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=amanda_weaver@berkeley.edu
+
+## Command(s) to run (example):
+module load r
+module load r-packages
+module load r-spatial
+R CMD BATCH dust_mask.R 
